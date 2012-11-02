128d85eced876b19eb0720183d680ec003fb0fda
Jon Gilkison <jon.gilkison@gmail.com>
2012-11-02 16:54:23 +0700

    Test case fixes for predicate query

--- Projects/Static Library/TestCases/Tests/MKitTC0002ModelTestCases.m
+++ Projects/Static Library/TestCases/Tests/MKitTC0002ModelTestCases.m
@@ -127,4 +127,66 @@
     STAssertTrue([m1.amodelArrayV indexOfObject:m2]!=NSNotFound, @"Model didn't deserialize correctly.");
 }
 
+
+-(void)test003PredicateQuery
+{
+    [MKitModelContext clearAllContexts];
+    
+    TestModel *m1=[self makeModelWithId:@"001"];
+    TestModel *m2=[self makeModelWithId:@"002"];
+    TestModel *m3=[self makeModelWithId:@"003"];
+    TestModel *m4=[self makeModelWithId:@"004"];
+    
+    // Note the circular references
+    m1.amodelArrayV=[MKitMutableModelArray arrayWithArray:@[m2,m3]];
+    m1.stringV=@"Jon";
+    m1.shortV=255;
+    m1.boolV=YES;
+    m1.amodelV=m4;
+    
+    m2.amodelArrayV=[MKitMutableModelArray arrayWithArray:@[m3,m4]];
+    m2.stringV=@"Jon";
+    m2.shortV=255;
+    m2.boolV=NO;
+    m2.amodelV=m1;
+    
+    m3.amodelArrayV=[MKitMutableModelArray arrayWithArray:@[m1,m2]];
+    m3.stringV=@"Chan";
+    m3.shortV=192;
+    m3.boolV=NO;
+    m3.amodelV=m1;
+    
+    m4.amodelArrayV=[MKitMutableModelArray arrayWithArray:@[m1,m2]];
+    m4.stringV=@"Jasan";
+    m4.shortV=192;
+    m4.boolV=YES;
+    m4.amodelV=m1;
+    
+    MKitModelQuery *query=[TestModel query];
+    [query key:@"shortV" condition:KeyEquals value:@(255)];
+    NSArray *results=[[query execute:nil] objectForKey:MKitQueryResultKey];
+    STAssertTrue(results.count==2, [NSString stringWithFormat:@"Should have 2 items, has %d",results.count]);
+    
+    query=[TestModel query];
+    [query key:@"amodelV" condition:KeyEquals value:m1];
+    results=[[query execute:nil] objectForKey:MKitQueryResultKey];
+    STAssertTrue(results.count==3, [NSString stringWithFormat:@"Should have 3 items, has %d",results.count]);
+
+    query=[TestModel query];
+    [query key:@"stringV" condition:KeyBeginsWith value:@"Jo"];
+    results=[[query execute:nil] objectForKey:MKitQueryResultKey];
+    STAssertTrue(results.count==2, [NSString stringWithFormat:@"Should have 2 items, has %d",results.count]);
+
+    query=[TestModel query];
+    [query key:@"stringV" condition:KeyEndsWith value:@"an"];
+    [query key:@"boolV" condition:KeyEquals value:@(YES)];
+    results=[[query execute:nil] objectForKey:MKitQueryResultKey];
+    STAssertTrue(results.count==1, [NSString stringWithFormat:@"Should have 1 items, has %d",results.count]);
+    
+    query=[TestModel query];
+    results=[[query execute:nil] objectForKey:MKitQueryResultKey];
+    STAssertTrue(results.count==4, [NSString stringWithFormat:@"Should have 4 items, has %d",results.count]);
+    
+}
+
 @end
