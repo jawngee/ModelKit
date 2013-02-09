6376fc1e413bd3dc6211ba8e3095e1c07ca973b6
Jon Gilkison <jon.gilkison@gmail.com>
2013-02-09 15:07:56 -0500

    Added a static lib with no dependencies.

---
+++ Static Library/ModelKitNoDep/ModelKitNoDep.h
@@ -0,0 +1,13 @@
+//
+//  ModelKitNoDep.h
+//  ModelKitNoDep
+//
+//  Created by Jon Gilkison on 12/22/12.
+//  Copyright (c) 2012 Interfacelab LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModelKitNoDep : NSObject
+
+@end
---
+++ Static Library/ModelKitNoDep/ModelKitNoDep.m
@@ -0,0 +1,13 @@
+//
+//  ModelKitNoDep.m
+//  ModelKitNoDep
+//
+//  Created by Jon Gilkison on 12/22/12.
+//  Copyright (c) 2012 Interfacelab LLC. All rights reserved.
+//
+
+#import "ModelKitNoDep.h"
+
+@implementation ModelKitNoDep
+
+@end
